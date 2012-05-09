9fccd72496f36e539d7af3cfd2fb8a5bb4dbd1bd
EthanZhu <pig.whose@gmail.com>
2012-05-09 13:55:10 +0800

    update

--- Scripts/global/login/require/main.js
+++ Scripts/global/login/require/main.js
@@ -3,9 +3,12 @@ define(['require','console.plugin','jquery','./startpage'], function(require) {
     //jquery have been loaded.
     var startpage = require('./startpage');
     var $ = require('jquery');
+    //var $ = jQuery;
     require(['console.plugin'],function(){
     	$(function() {
 			startpage.init();
+			console.log('cache size');
+			console.log($.cache);
 	    });
     })
 
